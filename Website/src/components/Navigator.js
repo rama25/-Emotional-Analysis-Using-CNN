cbc3f66b2160a97b625aed452d1165825c1b717e
smorriswisc <smorris24@wisc.edu>
2023-03-11 12:08:19 -0600

    Add UW logo

--- Website/src/components/Navigator.js
+++ Website/src/components/Navigator.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import uwLogo from '../Figures/UW.png';
 
 function Navigator() {
 
     return (
         <Container className='navigatorContainer myContainer' fluid="false">
+			<img src={uwLogo} alt="UW logo" class="myImage"></img>
             <h4><a href="#WelcomeContainer">Welcome</a></h4>
             <h4><a href="#PPContainer">Project Proposal</a></h4>
             <h4><a href="#ImplementationContainer">Implementation</a></h4>
