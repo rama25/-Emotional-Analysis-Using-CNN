9762ac7c5d7290489667ee45074fb592c5c1c86c
Maria <montes7@purdue.edu>
2023-03-12 00:07:09 -0600

    icluded images in webpage

--- Website/src/components/ImplementationScreen.js
+++ Website/src/components/ImplementationScreen.js
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import cnnPic from '../Figures/cnn_3.png';
+
 
 
 function ImplementationScreen() {
@@ -9,6 +11,17 @@ function ImplementationScreen() {
         <Container className='myContainer' id='ImplementationContainer'>
             <h1 className='purple'>Implementation</h1>
             <p>Our implementation was great because...</p>
+            <h3>CNN Architecture</h3>
+			<div className="welcomeSection">
+			<img src={cnnPic} alt="CNN Architecture" width={800}></img>
+				<p>
+				 This is a diagram that illustrated the architecture of our cnn model. 
+				</p>
+			</div>
+
+
+
+
         </Container>
 
     );
--- Website/src/components/WelcomeScreen.js
+++ Website/src/components/WelcomeScreen.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import silasPic from '../Figures/silas.jpg';
-import mariaPic from '../Figures/maria.jpg';
+import mariaPic from '../Figures/maria.png';
 
 function WelcomeScreen() {
 
@@ -11,14 +11,6 @@ function WelcomeScreen() {
     <Container className='myContainer' id='WelcomeContainer'>
 		<h1 className='green'>Welcome to our website</h1>
 		<h2>Meet the team:</h2>
-		<div>
-			<h3>Maria Elisa Montes Gonzalez</h3>
-			<div className="welcomeSection">
-				<p>
-				 We can may be add our pictures and small intro :)
-				</p>
-			</div>
-		</div>
 		<div>
 			<h3>Ramapriya Ranganath</h3>
 			<div className="welcomeSection">
@@ -34,7 +26,10 @@ function WelcomeScreen() {
 				<p>
 				 I'm a second year CS Masters student at UW and I'm hoping to graduate in May 2023. I moved to Madison in the summer of 2016 from Conroe, Texas and I'm currently working as a software developer at Epic.
 				</p>
-				<img src={mariaPic} alt="Picture of Maria" className="welcomeImage"></img>
+			</div>
+			<h3>Maria Elisa Montes</h3>
+			<div className="welcomeSection">
+			<img src={mariaPic} alt="Picture of Maria" className="welcomeImage"></img>
 				<p>
 				 Hi my name is Maria, I am originally form center Mexico. I came to Madison last January to start a PhD. in animal science focusing on precision livestock farming. 
 				</p>
