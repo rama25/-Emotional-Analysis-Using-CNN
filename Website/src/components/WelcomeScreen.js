6ab25ac81b85e006e05c0bfad41d5a7c0763dca2
smorriswisc <smorris24@wisc.edu>
2023-04-25 21:08:21 -0500

    Remove maria

--- Website/src/components/WelcomeScreen.js
+++ Website/src/components/WelcomeScreen.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import silasPic from '../Figures/silas.jpg';
 import ramaPic from '../Figures/rama.jpg';
-import mariaPic from '../Figures/maria.png';
 
 function WelcomeScreen() {
 
@@ -29,13 +28,6 @@ function WelcomeScreen() {
 				<img src={silasPic} alt="Picture of Silas" className="welcomeImage"></img>
 				<p>
 				 I'm a second year CS Masters student at UW and I'm hoping to graduate in May 2023. I moved to Madison in the summer of 2016 from Conroe, Texas and I'm currently working as a software developer at Epic.
-				</p>
-			</div>
-			<h3>Maria Elisa Montes</h3>
-			<div className="welcomeSection">
-			<img src={mariaPic} alt="Picture of Maria" className="welcomeImage"></img>
-				<p>
-				 Hi my name is Maria. I grew up in Guanajuato, Mexico where I got my bachelors in Agronomy Engineering. Last January I came to Madison to start a PhD. in Animal Sciences focusing on precision livestock farming, specifically with dairy cows. In my free time I dance ballet and train gymnastics.
 				</p>
 			</div>
 		</div>
